fe93867840c5457204fb4890a49d1a6c3b595d52
wenhao555 <710278488@qq.com>
2020-01-08 22:53:40 +0800

    gson使用

--- app/src/main/java/com/example/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.java
@@ -5,16 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity
-{
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String string = "]\"}";
-        string = string.replaceAll("]\"", "]");
-        Log.e("ad", string);
+        strToJson();
+
+    }
+
+    private void strToJson() {
+        JsonArray array = new JsonArray();
+        JsonObject object = new JsonObject();
+        JsonObject obj = new JsonObject();
+        object.addProperty("name", "ta");
+        object.addProperty("name2", "ta2");
+        array.add(object);
+        obj.addProperty("a1", new Gson().toJson(array));
+        Log.e("la", obj.toString());
+        //查看数组转字符串
     }
 }
